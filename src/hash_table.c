b241b90669d7d92c331b7c84557bb9bd90ab71fe
Ky Nguyen <kynguyen@kynguyen-macbookpro.roam.corp.google.com>
2018-12-12 00:42:58 -0800

    Adds collision resolution function

--- src/hash_table.c
+++ src/hash_table.c
@@ -4,6 +4,9 @@
 
 #include "hash_table.h"
 
+const int HT_PRIME_1 = 223;
+const int HT_PRIME_2 = 277;
+
 static ht_item* ht_new_item(const char* k, const char* v) {
     ht_item* i = malloc(sizeof(ht_item));
     i->key = strdup(k);
@@ -47,4 +50,12 @@ static int ht_hash(const char* s, const int a, const int m) {
         hash = hash % m;
     }
     return (int)hash;
+}
+
+static int ht_get_hash(
+    const char* s, const int num_buckets, const int attempt
+) {
+    const int hash_a = ht_hash(s, HT_PRIME_1, num_buckets);
+    const int hash_b = ht_hash(s, HT_PRIME_2, num_buckets);
+    return (hash_a + (attempt * (hash_b + 1))) % num_buckets;
 }
